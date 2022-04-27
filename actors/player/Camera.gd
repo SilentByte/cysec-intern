9ceba0945f440426b1691edfe921fdc5f5ac9574
Rico A. Beti <rico.beti@silentbyte.com>
2022-04-27 20:25:53 +0800

    Implement basic player controller

---
+++ actors/player/Camera.gd
@@ -0,0 +1,29 @@
+extends Camera2D
+
+export var min_zoom := 0.25
+export var max_zoom := 5.0
+export var zoom_offset := 0.5
+export var zoom_duration := 0.5
+
+onready var tween: Tween = $Tween
+var _zoom_level := 1.0
+
+func _set_zoom_level(value: float) -> void:
+	_zoom_level = clamp(value, min_zoom, max_zoom)
+	tween.interpolate_property(
+		self,
+		"zoom",
+		zoom,
+		Vector2(_zoom_level, _zoom_level),
+		zoom_duration,
+		tween.TRANS_SINE,
+		tween.EASE_OUT
+	)
+	tween.start()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("zoom_in"):
+		_set_zoom_level(_zoom_level - zoom_offset)
+
+	if event.is_action_pressed("zoom_out"):
+		_set_zoom_level(_zoom_level + zoom_offset)
---
+++ actors/player/Player.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var movement_direction = Vector2.ZERO
+var facing_direction = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	movement_direction = Vector2.ZERO
+
+	if Input.is_action_pressed("move_east"):
+		movement_direction.x += 1.0
+
+	if Input.is_action_pressed("move_west"):
+		movement_direction.x -= 1.0
+
+	if Input.is_action_pressed("move_north"):
+		movement_direction.y -= 1.0
+
+	if Input.is_action_pressed("move_south"):
+		movement_direction.y += 1.0
+
+	_update_animation()
+	position += movement_direction.normalized() * 100 * delta
+
+func _update_animation():
+	if movement_direction.x > 0:
+		$AnimatedSprite.animation = "east_walk"
+	elif movement_direction.x < 0:
+		$AnimatedSprite.animation = "west_walk"
+	elif movement_direction.y < 0:
+		$AnimatedSprite.animation = "north_walk"
+	elif movement_direction.y > 0:
+		$AnimatedSprite.animation = "south_walk"
+	else:
+		$AnimatedSprite.animation = "east_idle"
