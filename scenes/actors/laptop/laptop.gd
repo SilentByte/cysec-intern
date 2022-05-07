5d4d3ce897f1855650b369850b5f56501dd7bff4
Rico A. Beti <rico.beti@silentbyte.com>
2022-05-08 00:39:43 +0800

    Implement laptop/exe dialog

--- scenes/actors/laptop/laptop.gd
+++ scenes/actors/laptop/laptop.gd
@@ -53,6 +53,62 @@ var dialog_content = {
 			"""
 		),
 	},
+	"exe":
+	{
+		"$begin":
+		Utils.dialog_part(
+			"""
+			Ah, it's so nice to sit outside and <~relax a bit~>.
+
+			Looks like my buddy Jake sent me a chat message:
+
+			<#Hey, zup! I've been playing this crazy new video
+			game, it's so competitive! I'm sending it to
+			you, let's play!
+
+			Sending starbattleroyale.exe ... ... ... ... ...
+			File Transfer Completed.#>
+
+			<?[url=$accept:bad]1) Awesome! Let's gooooooo![/url]?>
+
+			<?[url=$reject:good]2) Thanks, but I'm going to get my own copy later.[/url]?>
+			"""
+		),
+		"$accept":
+		Utils.dialog_part(
+			"""
+			<!That was extremely risky!>
+
+			You have received an executable file. While Jake may
+			have good intensions, the EXE file might be infected
+			with malicious software.
+
+			You should be extremely careful when receiving
+			executable files. <!Don't just open them!!>
+
+			[center]<#[url=https://blog.malwarebytes.com/explained/2021/10/what-is-an-exe-file-is-it-the-same-as-an-executable/]Click here to learn more.[/url]#>[/center]
+
+			<?[url=$end]CONTINUE[/url]?>
+			"""
+		),
+		"$reject":
+		Utils.dialog_part(
+			"""
+			<~Yes, it's good to be on the safe side!~>
+
+			You have received an executable file. While Jake may
+			have good intensions, the EXE file might be infected
+			with malicious software.
+
+			You should be extremely careful when receiving
+			executable files. <!Don't just open them!!>
+
+			[center]<#[url=https://blog.malwarebytes.com/explained/2021/10/what-is-an-exe-file-is-it-the-same-as-an-executable/]Click here to learn more.[/url]#>[/center]
+
+			<?[url=$end]CONTINUE[/url]?>
+			"""
+		),
+	},
 }
 
 
