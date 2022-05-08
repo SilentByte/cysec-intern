19dc59fa0bf741fe124a04fa284f2a7104326ead
Rico A. Beti <rico.beti@silentbyte.com>
2022-05-08 13:54:35 +0800

    Implement boss

---
+++ scenes/actors/boss/boss.gd
@@ -0,0 +1,61 @@
+extends KinematicBody2D
+
+onready var hud := $"/root/Main/Hud"
+onready var audio := $AudioStreamPlayer
+onready var poi := $PointOfInterest
+
+var _last_total_score := -1.0
+
+
+func interact() -> void:
+	if audio.playing:
+		return
+
+	audio.play()
+	_last_total_score = Facts.total_score()
+
+	var content := ""
+	if Facts.scored_interaction_count() < Facts.TOTAL_SCORABLE_INTERACTION_COUNT:
+		content = (
+			"""
+			<~Hey, my new favorite intern! :-)~>
+
+			So far, you have found <!%s out of %s challenges!>,
+			with an <!overall score of %s%%!>.
+
+			Keep at it! There's more to learn.
+
+			<?[url=$end]CONTINUE[/url]?>
+			"""
+			% [
+				Facts.scored_interaction_count(),
+				Facts.TOTAL_SCORABLE_INTERACTION_COUNT,
+				round(Facts.total_score() * 100.0),
+			]
+		)
+	else:
+		content = (
+			"""
+			<~Hey, my new favorite intern! :-)~>
+
+			Amazing! Looks like you have found <!ALL %s!> challenges!
+			and reached an <!overall score of %s%%!>.
+
+			<?[url=$end]CONTINUE[/url]?>
+			"""
+			% [
+				Facts.TOTAL_SCORABLE_INTERACTION_COUNT,
+				round(Facts.total_score() * 100.0),
+			]
+		)
+
+	hud.show_dialog(
+		"boss",
+		{
+			"$begin": Utils.dialog_part(content),
+		}
+	)
+
+
+func _physics_process(delta: float) -> void:
+	poi.visible = _last_total_score != Facts.total_score()
--- scripts/global/facts.gd
+++ scripts/global/facts.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const TOTAL_SCORABLE_INTERACTION_COUNT := 8
+
 var _last_interaction := ""
 var _had_interactions := {}
 
@@ -19,6 +21,48 @@ func good_interaction() -> void:
 func bad_interaction() -> void:
 	if _had_interactions[_last_interaction] == 0:
 		_had_interactions[_last_interaction] = -1
+
+
+func scored_interaction_count() -> int:
+	var count := 0
+
+	for v in _had_interactions.values():
+		if v != 0:
+			count += 1
+
+	return count
+
+
+func completion_ratio() -> float:
+	return float(scored_interaction_count()) / float(TOTAL_SCORABLE_INTERACTION_COUNT)
+
+
+func good_bad_ratio() -> float:
+	if scored_interaction_count() == 0:
+		return 0.0
+
+	var good_sum := 0.0
+	var bad_sum := 0.0
+
+	for v in _had_interactions.values():
+		if v > 0:
+			good_sum += 1
+		elif v < 0:
+			bad_sum += 1
+
+	if bad_sum == 0:
+		return 1.0
+
+	return good_sum / bad_sum
+
+
+func total_score() -> float:
+	var good_count := 0
+	for v in _had_interactions.values():
+		if v > 0:
+			good_count += 1
+
+	return float(good_count) / float(TOTAL_SCORABLE_INTERACTION_COUNT)
 
 
 func has_had_interaction(interaction: String) -> bool:
